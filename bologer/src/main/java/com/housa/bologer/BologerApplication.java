9c7a55cd8a0ceeca61acb947f9b2cc340cb0ec68
helbadao <elbadaouihoussam4@gmail.com>
2026-04-21 10:13:19 +0100

    intialaze my blog

---
+++ bologer/src/main/java/com/housa/bologer/BologerApplication.java
@@ -0,0 +1,13 @@
+package com.housa.bologer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BologerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BologerApplication.class, args);
+	}
+
+}
